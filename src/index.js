e7f8d9847da7e47e06acf0930a13a314ed7a7b52
ERICKGZ87 <erickgz87@gmail.com>
2021-11-21 22:38:14 +0000

    lista tareas

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const DATA = [
+  { id: "todo-0", name: "Eat888", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
+
+ReactDOM.render(
+  
+    <App tasks={DATA} />
+  ,
+  document.getElementById('root')
+);
+
